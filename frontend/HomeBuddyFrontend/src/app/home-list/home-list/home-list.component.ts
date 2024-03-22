dc5ed561c9f5079e6d53fba61c292e9800e131b0
ntmartin <ntmartin@ncsu.edu>
2024-03-22 07:35:25 -0400

    basic home entry

---
+++ frontend/HomeBuddyFrontend/src/app/home-list/home-list/home-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-list.component.html',
+  styleUrl: './home-list.component.css',
+})
+export class HomeListComponent {}
